8a1e4d0808059a1fbd2813f3da6084b21e8503b6
Eric Lang <94770511+88potatoes@users.noreply.github.com>
2024-08-03 02:45:40 +1000

    incrementing and decrementing works

--- src/pages/MatchInfo.tsx
+++ src/pages/MatchInfo.tsx
@@ -1,4 +1,5 @@
 import FighterCard from "@/components/FighterCard";
+import ScoreCard from "@/components/ScoreCard";
 import { Button } from "@/components/ui/button";
 import { Card, CardDescription, CardTitle } from "@/components/ui/card";
 import { Match } from "@/types/types";
@@ -17,16 +18,18 @@ export default function MatchInfo() {
 
   useEffect(() => {
     if (location.state && location.state.match) {
-        setMatch(location.state.match);
+      setMatch(location.state.match);
     } else {
-        //
+      // TODO : to add the fetch
     }
   }, []);
 
   if (match === null) {
-    return <div>
+    return (
+      <div>
         <h1>Error occured.</h1>
-    </div>
+      </div>
+    );
   }
 
   return (
@@ -37,19 +40,10 @@ export default function MatchInfo() {
       <div className="flex flex-row justify-around">
         <Card className="p-2">
           <FighterCard fighter={match.fighter1} />
-          <div className="pt-2">
-            <CardTitle>Score</CardTitle>
-            <CardDescription>
-              Punches Thrown: {match.scores.fighter1.thrown}
-            </CardDescription>
-            <CardDescription>
-              Punches Hit: {match.scores.fighter1.hits}
-            </CardDescription>
-            <CardDescription>
-              Punches Hit %:{" "}
-              {((match.scores.fighter1.hits / match.scores.fighter1.thrown) * 100).toFixed(1)}%
-            </CardDescription>
-          </div>
+          <ScoreCard
+            thrown={match.scores.fighter1.thrown}
+            hits={match.scores.fighter1.hits}
+          />
           <div>
             <div className="mt-2">
               <CardTitle className="text-sm">Punches Thrown</CardTitle>
@@ -71,19 +65,10 @@ export default function MatchInfo() {
         {/* DUPLICATE */}
         <Card className="p-2">
           <FighterCard fighter={match.fighter2} />
-          <div className="pt-2">
-            <CardTitle>Score</CardTitle>
-            <CardDescription>
-              Punches Thrown: {match.scores.fighter2.thrown}{" "}
-            </CardDescription>
-            <CardDescription>
-              Punches Hit: {match.scores.fighter2.hits}
-            </CardDescription>
-            <CardDescription>
-              Punches Hit %:{" "}
-              {((match.scores.fighter2.hits / match.scores.fighter2.thrown) * 100).toFixed(1)}%
-            </CardDescription>
-          </div>
+          <ScoreCard
+            thrown={match.scores.fighter2.thrown}
+            hits={match.scores.fighter2.hits}
+          />
           <div>
             <div className="mt-2">
               <CardTitle className="text-sm">Punches Thrown</CardTitle>
--- src/pages/NewMatchLive.tsx
+++ src/pages/NewMatchLive.tsx
@@ -2,15 +2,63 @@ import FighterCard from "@/components/FighterCard";
 import ScoreCard from "@/components/ScoreCard";
 import { Button } from "@/components/ui/button";
 import { Card, CardDescription, CardTitle } from "@/components/ui/card";
-import { MutableRefObject, useEffect, useRef } from "react";
+import { MutableRefObject, useEffect, useRef, useState } from "react";
 /**
  * The page for the Main view with the camera and the live scoring
  * @returns 
  */
 //
+
+const fighterPreset1 = {
+    id: 1,
+    name: "Fighter Uno",
+    country: "Australia",
+    avatarURL: "https://wallpapers.com/images/hd/pfp-pictures-t0vlqv5glu7xo4mb.jpg"
+}
+const fighterPreset2 = {
+    id: 2,
+    name: "Fighter Duo",
+    country: "New Zealand",
+    avatarURL: "https://wallpapers-clan.com/wp-content/uploads/2023/01/anime-aesthetic-boy-pfp-3.jpg"
+}
+
+
 export default function NewMatchLive() {
     const videoRef: MutableRefObject<HTMLVideoElement | null> = useRef(null);
+    const [fighter1thrown, setFighter1thrown] = useState(0);
+    const [fighter1hits, setFighter1hits] = useState(0);
+    const [fighter2thrown, setFighter2thrown] = useState(0);
+    const [fighter2hits, setFighter2hits] = useState(0);
+    const [fighter1, setFighter1] = useState(fighterPreset1);
+    const [fighter2, setFighter2] = useState(fighterPreset2);
 
+    const incrementF1thrown = () => {
+        setFighter1thrown(fighter1thrown + 1);
+    }
+    const incrementF2thrown = () => {
+        setFighter2thrown(fighter2thrown + 1);
+    }
+    const decrementF1thrown = () => {
+        setFighter1thrown(Math.max(0, fighter1thrown - 1));
+    }
+    const decrementF2thrown = () => {
+        setFighter2thrown(Math.max(0, fighter2thrown - 1));
+    }
+
+    const incrementF1hits = () => {
+        setFighter1hits(fighter1hits + 1);
+    }
+    const incrementF2hits = () => {
+        setFighter2hits(fighter2hits + 1);
+    }
+    const decrementF1hits = () => {
+        setFighter1hits(Math.max(0, fighter1hits - 1));
+    }
+    const decrementF2hits = () => {
+        setFighter2hits(Math.max(0, fighter2hits - 1));
+    }
+
+    // camera
     useEffect(() => {
         // check for browser support
         if (!navigator.mediaDevices) {
@@ -37,53 +85,42 @@ export default function NewMatchLive() {
         </div>
         <div className="flex flex-row justify-around">
             <Card className="p-2">
-                <FighterCard/>
-                <ScoreCard thrown={10} hits={10}/>
+                <FighterCard fighter={fighter1}/>
+                <ScoreCard thrown={fighter1thrown} hits={fighter1hits}/>
                 <div>
                     <div className="mt-2">
                         <CardTitle className="text-sm">Punches Thrown</CardTitle>
                         <div>
-                            <Button>+</Button>
-                            <Button>-</Button>
-
+                            <Button onClick={incrementF1thrown}>+</Button>
+                            <Button onClick={decrementF1thrown}>-</Button>
                         </div>
                     </div>
                     <div className="mt-2">
                         <CardTitle className="text-sm">Punches Hit</CardTitle>
                         <div>
-                            <Button>+</Button>
-                            <Button>-</Button>
-
+                            <Button onClick={incrementF1hits}>+</Button>
+                            <Button onClick={decrementF1hits}>-</Button>
                         </div>
                     </div>
                 </div>
             </Card>
 
-            {/* DUPLICATE */}
             <Card className="p-2">
-                <FighterCard/>
-                <div className="pt-2">
-                    
-                    <CardTitle>Score</CardTitle>
-                    <CardDescription>Punches Thrown: 10</CardDescription>
-                    <CardDescription>Punches Hit: 10</CardDescription>
-                    <CardDescription>Punches Hit %: 10</CardDescription>
-                </div>
+                <FighterCard fighter={fighter2}/>
+                <ScoreCard thrown={fighter2thrown} hits={fighter2hits}/>
                 <div>
                     <div className="mt-2">
                         <CardTitle className="text-sm">Punches Thrown</CardTitle>
                         <div>
-                            <Button>+</Button>
-                            <Button>-</Button>
-
+                            <Button onClick={incrementF2thrown}>+</Button>
+                            <Button onClick={decrementF2thrown}>-</Button>
                         </div>
                     </div>
                     <div className="mt-2">
                         <CardTitle className="text-sm">Punches Hit</CardTitle>
                         <div>
-                            <Button>+</Button>
-                            <Button>-</Button>
-
+                            <Button onClick={incrementF2hits}>+</Button>
+                            <Button onClick={decrementF2hits}>-</Button>
                         </div>
                     </div>
                 </div>
